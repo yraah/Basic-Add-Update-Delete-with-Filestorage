1004d6711ac1326aa143b2c08fc7a621bbd59cdd
Yraah <harry.santos@gmail.com>
2024-11-19 19:35:19 +0800

    ADD add more details for setup

---
+++ BpiBackend/Table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE user_accounts (
+  id SERIAL PRIMARY KEY,
+  photo TEXT,
+  name VARCHAR(100),
+  username VARCHAR(50) UNIQUE NOT NULL,
+  country VARCHAR(50),
+  email VARCHAR(100) UNIQUE NOT NULL,
+  account_type VARCHAR(20),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
