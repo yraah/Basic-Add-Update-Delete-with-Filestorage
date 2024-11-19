df9ab63541f420753e6b2ac9d6913d9dabf49a5d
Harry Santos <53293031+yraah@users.noreply.github.com>
2024-11-19 20:28:11 +0800

    Update Table.sql

--- BpiBackend/Table.sql
+++ BpiBackend/Table.sql
@@ -2,9 +2,9 @@ CREATE TABLE user_accounts (
   id SERIAL PRIMARY KEY,
   photo TEXT,
   name VARCHAR(100),
-  username VARCHAR(50) UNIQUE NOT NULL,
+  username VARCHAR(50),
   country VARCHAR(50),
-  email VARCHAR(100) UNIQUE NOT NULL,
+  email VARCHAR(100),
   account_type VARCHAR(20),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
